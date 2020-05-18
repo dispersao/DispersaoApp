5a4d217e07734fd751aeb02f93b742c98f3ee91d
bruno vianna <bruno@pobox.com>
2020-05-18 14:58:33 +0200

    added keyboard avoiding view (squeeze logo)

--- src/screens/Token/index.jsx
+++ src/screens/Token/index.jsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { StyleSheet } from 'react-native'
-import Constants from 'expo-constants'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import Constants from "expo-constants";
+import { StyleSheet, KeyboardAvoidingView } from "react-native";
 
-import {
-  Container,
-  Content,
-} from 'native-base'
+import { Container, Content } from "native-base";
 
-import {
-  Row,
-  Grid
-} from 'react-native-easy-grid'
+import { Row, Grid } from "react-native-easy-grid";
 
-import { toJS } from '../../utils/immutableToJs.jsx'
+import { toJS } from "../../utils/immutableToJs.jsx";
 
 import Logo from '../../components/logo'
 import TokenCheck from './components/TokenCheck.jsx'
@@ -34,58 +28,56 @@ const TokenScreen = ({
   }, [userScript])
 
   return (
-    <Container style={styles.container} >
-      <Content contentContainerStyle={styles.contentStyles}>
-        <Grid style={styles.grid} >
-          <Row size={1} />
-          <Row size={5}>
-            <Logo />
-          </Row>
-          <TokenCheck />
-        </Grid>
-      </Content>
+    <Container style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <Content contentContainerStyle={styles.contentStyles}>
+          <Grid style={styles.grid}>
+            <Row size={1} />
+            <Row size={6}>
+              <Logo />
+            </Row>
+            <TokenCheck />
+          </Grid>
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'black',
+    height: "100%",
+    width: "100%",
+    backgroundColor: "black",
     paddingHorizontal: 10,
   },
   contentStyles: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'black',
-    height: '100%'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+    height: "100%",
   },
   paddingRow: {
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   grid: {
-    alignItems: 'center'
+    alignItems: "center",
   },
   textRow: {
-    justifyContent: 'flex-start',
-    alignItems: 'center'
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
   text: {
-    fontSize: 15, 
-    color: '#D96235', 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  }
-})
-
+    fontSize: 15,
+    color: "#D96235",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 const mapStateToProps = (state) => ({
   userScript: getAppuserScript(state)
 })
 
-export default connect(
-  mapStateToProps,
-  null
-)(toJS(TokenScreen))
+export default connect(mapStateToProps, null)(toJS(TokenScreen));
